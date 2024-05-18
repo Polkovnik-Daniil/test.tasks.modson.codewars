0cf26521c387b341a629d9150946f2561b34e199
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-18 17:30:57 +0300

    finish 13 task

--- index.ts
+++ index.ts
@@ -1,21 +1,4 @@
-function stringParse(row: string) {
-  if (typeof row !== "string") return "Please enter a valid string";
-  let arr: string[] = [];
-  let str: string = "";
-  for (let i = 0; i < row.length; i++) {
-    if (row[i - 1] === row[i]) {
-      str += row[i];
-    } else {
-      arr.push(str);
-      str = row[i];
-    }
-    if (i === row.length - 1) {
-      arr.push(str);
-    }
-  }
-  return arr
-    .map((v) => (v.length > 2 ? v.slice(0, 2) + "[" + v.slice(2) + "]" : v))
-    .join("");
+function breakCamelCase(string: string) {
+  return(string.replace(/([A-Z])/g, ' $1'));
 }
-
-console.log(stringParse("aaaabbcdefffffffg"));
+console.log(breakCamelCase('camelCasing'));
