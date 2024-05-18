c6d3fe837f81ba26e03cd1bd967bb570b192acb3
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-18 18:44:23 +0300

    finish 19 task

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
-function duplicArg(...arr: any) {
-  return [...new Set(arr)].length != arr.length;
+function fatFing(str: string) {
+  if (typeof str !== "string") return str;
+  return str
+    .replace(/A/g, "a")
+    .split("a")
+    .map((s, i) =>
+      i % 2
+        ? s.replace(/./g, (m) =>
+            /[a-z]/.test(m) ? m.toUpperCase() : m.toLowerCase()
+          )
+        : s
+    )
+    .join("");
 }
 
-console.log(duplicArg(1, 2, 3, 4, 5));
+console.log(fatFing("The quick brown fox jumps over the lazy dog."));
