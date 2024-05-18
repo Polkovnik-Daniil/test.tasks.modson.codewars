7a9fe0fe5499a40593ef262f94fe76a4588c27d0
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-18 18:54:44 +0300

    finish 20 task

--- index.ts
+++ index.ts
@@ -1,16 +1,5 @@
-function fatFing(str: string) {
-  if (typeof str !== "string") return str;
-  return str
-    .replace(/A/g, "a")
-    .split("a")
-    .map((s, i) =>
-      i % 2
-        ? s.replace(/./g, (m) =>
-            /[a-z]/.test(m) ? m.toUpperCase() : m.toLowerCase()
-          )
-        : s
-    )
-    .join("");
+function doorsInSchool(n: number) {
+  return ~~Math.sqrt(n);
 }
 
-console.log(fatFing("The quick brown fox jumps over the lazy dog."));
+console.log(doorsInSchool(5));
