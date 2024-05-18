e7ed5263a1aadd4712ea1133042f18084ddcea45
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-18 11:18:06 +0300

    finish 5 task

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Задача которая была выполнена в данном коммите: https://www.codewars.com/kata/52ea928a1ef5cfec800003ee
+Задача которая была выполнена в данном коммите: https://www.codewars.com/kata/54da5a58ea159efa38000836
--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
-function ip_to_int32(ip: string) {
-  return ip.split(".").reduce((int, v) => 256 * int + Number(v), 0);
+function groupByNumber(array: number[]) {
+  let map = new Map();
+  for (let e of array) {
+      map.set(e, map.has(e) ? map.get(e) + 1 : 1);
+  }
+  return [...map];
 }
-console.log(ip_to_int32("128.32.10.1"));
+function findOdd(a: number[]) {
+  let groups = groupByNumber(a);
+  return groups.find(e => e[1] % 2)![0];
+}
+
+console.log(findOdd([7]));
+console.log(findOdd([0]));
+console.log(findOdd([1, 1, 2]));
+console.log(findOdd([0, 1, 0, 1, 0]));
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
