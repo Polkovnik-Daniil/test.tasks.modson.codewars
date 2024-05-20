a0500a6fb70c915519d6174352e30025b1a0fc9d
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-20 19:39:59 +0300

    finish 33 task

--- index.ts
+++ index.ts
@@ -1,15 +1,2 @@
-Math.round = function(number: number) {
-  return (number - parseInt(number.toString()) >= 0.5) ? parseInt(number.toString()) + 1 : parseInt(number.toString()) ;
-};
-
-Math.ceil = function(number: number) {
-  return (parseInt(number.toString()) === number) ? number : parseInt(number.toString()) + 1;
-};
-
-Math.floor = function(number: number) {
-  return parseInt(number.toString());
-};
-
-console.log(Math.floor(5.999));
-console.log(Math.ceil(5.999));
-console.log(Math.round(5.999));
+var Rot13 = (row: string): string => row.replace(/[a-z]/gi, letter => String.fromCharCode(letter.charCodeAt(0) + (letter.toLowerCase() <= 'm' ? 13: -13)));
+console.log(Rot13("Hello world!"));
