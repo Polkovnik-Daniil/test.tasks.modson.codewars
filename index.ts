bced0513fd4f59470c3e5e99142e83b9b5d39f6d
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-20 19:54:09 +0300

    finish 34 task

--- index.ts
+++ index.ts
@@ -1,2 +1,12 @@
-var Rot13 = (row: string): string => row.replace(/[a-z]/gi, letter => String.fromCharCode(letter.charCodeAt(0) + (letter.toLowerCase() <= 'm' ? 13: -13)));
-console.log(Rot13("Hello world!"));
+function validParentheses(parenths: string) {
+  if (parenths.length % 2 != 0) return false;
+  let str = parenths.slice();
+  for (let i = 0; i <= str.length; i++) {
+    str = str.replace(/\(\)/g, "");
+  }
+  return str.replace(/\(\)/g, "") === "";
+}
+console.log(validParentheses("()"));
+console.log(validParentheses(")(()))"));
+console.log(validParentheses("("));
+console.log(validParentheses("(())((()())())"));
