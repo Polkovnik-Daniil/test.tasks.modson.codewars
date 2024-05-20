fae630f2ece60e77bb713da5569ac9da84cb9476
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-20 21:00:53 +0300

    finish 37 task

--- index.ts
+++ index.ts
@@ -1,35 +1,9 @@
-namespace test.task {
-  export class Lock {
-    keys: Key[];
-    constructor() {
-      this.keys = [];
-    }
-    createKey() {
-      var key = new Key((Math.random() + 1).toString(36).substring(7));
-      this.keys.push(key);
-      return key;
-    }
-    check(key: Key) {
-      return this.keys.some((local_key) => local_key.id === key.id);
-    }
-    expire(key: Key) {
-      this.keys.splice(this.keys.indexOf(key), 1);
-    }
-  }
-  class Key {
-    id: string;
-    constructor(id: string) {
-      this.id = id;
-    }
-  }
+function humanReadable(seconds: number) {
+  var hours = parseInt((seconds / 3600).toString());
+  var minutes = parseInt((seconds / 60).toString()) % 60;
+  var seconds = seconds % 60;
+
+  var dozen = (value: number) => (value < 10 ? "0" + value : value);
+  return dozen(hours) + ":" + dozen(minutes) + ":" + dozen(seconds);
 }
-
-var lock1 = new test.task.Lock();
-var key1 = lock1.createKey();
-console.log(lock1.check(key1)); // true
-lock1.expire(key1);
-console.log(lock1.check(key1)); // false
-
-var lock2 = new test.task.Lock();
-var key2 = lock2.createKey();
-console.log(lock1.check(key2)); // false
+console.log(humanReadable(359999));
