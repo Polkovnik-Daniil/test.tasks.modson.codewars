ef6f3340f39e077057ce26feac2ed12b763ee8bc
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-20 18:02:53 +0300

    finish 30 task

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Задача которая была выполнена в данном коммите: https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39
+Задача которая была выполнена в данном коммите: https://www.codewars.com/kata/526dad7f8c0eb5c4640000a4
--- index.ts
+++ index.ts
@@ -1,56 +1,78 @@
-function zero(func?: any) {
-  return func ? func(0) : 0;
+class Vector {
+  array!: number[];
+  constructor(value: number[]) {
+    this.array = value;
+  }
 }
-function one(func?: any) {
-  return func ? func(1) : 1;
-}
-function two(func?: any) {
-  return func ? func(2) : 2;
-}
-function three(func?: any) {
-  return func ? func(3) : 3;
-}
-function four(func?: any) {
-  return func ? func(4) : 4;
-}
-function five(func?: any) {
-  return func ? func(5) : 5;
-}
-function six(func?: any) {
-  return func ? func(6) : 6;
-}
-function seven(func?: any) {
-  return func ? func(7) : 7;
-}
-function eight(func?: any) {
-  return func ? func(8) : 8;
-}
-function nine(func?: any) {
-  return func ? func(9) : 9;
+interface Vector {
+  add(vect: Vector): Vector;
+  subtract(vect: Vector): Vector;
+  dot(vect: Vector): number;
+  equals(vect: Vector): boolean;
+  norm(): number;
 }
 
-function plus(num2: number) {
-  return function (num1: number) {
-    return num1 + num2;
-  };
-}
-function minus(num2: number) {
-  return function (num1: number) {
-    return num1 - num2;
-  };
-}
-function times(num2: number) {
-  return function (num1: number) {
-    return num1 * num2;
-  };
-}
-function dividedBy(num2: number) {
-  return function (num1: number) {
-    return num1 / num2;
-  };
-}
+Vector.prototype.add = function (vect: Vector) {
+  var a = this.array;
+  var vect_arr = vect.array;
+  if (a.length !== vect_arr.length)
+    throw new TypeError("Vectors have different length");
+  return new Vector(
+    a.map(function (x, i) {
+      return x + vect_arr[i];
+    })
+  );
+};
 
-console.log(seven(times(five()))); // must return 35
-console.log(four(plus(nine()))); // must return 13
-console.log(eight(minus(three()))); // must return 5
-console.log(six(dividedBy(two()))); // must return 3
+Vector.prototype.subtract = function (vect: Vector) {
+  var array = this.array;
+  var vect_arr = vect.array;
+  if (array.length !== vect_arr.length)
+    throw new TypeError("Vectors have different length");
+  return new Vector(
+    array.map(function (x, i) {
+      return x - vect_arr[i];
+    })
+  );
+};
+
+Vector.prototype.dot = function (vect: Vector) {
+  var array = this.array;
+  var vect_arr = vect.array;
+  if (array.length !== vect_arr.length)
+    throw new TypeError("Vectors have different length");
+  return array.reduce(function (s, x, i) {
+    return s + x * vect_arr[i];
+  }, 0);
+};
+
+Vector.prototype.equals = function (vect: Vector) {
+  var array = this.array;
+  var vect_arr = vect.array;
+  if (array.length !== vect_arr.length) return false;
+  return array.every(function (x, i) {
+    return x === vect_arr[i];
+  });
+};
+
+Vector.prototype.norm = function () {
+  var array = this.array;
+  return Math.sqrt(
+    array.reduce(function (s, x) {
+      return s + x * x;
+    }, 0)
+  );
+};
+
+// Vector.prototype.toString = function () {
+//   return "(" + this.x.join(",") + ")";
+// };
+
+var a = new Vector([1, 2, 3]);
+var b = new Vector([3, 4, 5]);
+var c = new Vector([5, 6, 7, 8]);
+console.log(a.add(b)); // should return a new Vector([4, 6, 8]))
+console.log(a.subtract(b)); // should return a new Vector([-2, -2, -2])
+console.log(a.dot(b)); // should return 1*3 + 2*4 + 3*5 = 26
+console.log(a.norm()); // should return sqrt(1^2 + 2^2 + 3^2) = sqrt(14)
+console.log(a.add(c)); // throws an error
