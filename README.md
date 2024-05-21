86262cdc82fa9cab946eb63818580cd29e102f7f
Polkovnik-Daniil <0205danik@mail.ru>
2024-05-21 22:33:39 +0300

    finish 49 task

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Задача которая была выполнена в данном коммите: https://www.codewars.com/kata/528d9adf0e03778b9e00067e
+Задача которая была выполнена в данном коммите: https://www.codewars.com/kata/515decfd9dcfc23bb6000006
--- index.ts
+++ index.ts
@@ -1,21 +1,11 @@
-function mineLocation(field: number[][]): number[] {
-  let posX: number = -1;
-  let posY: number = -1;
-  field.map((valX, indexX) =>
-    valX.map((valueY, indexY) => {
-      if (valueY === 1) {
-        posX = indexX;
-        posY = indexY;
-      }
-    })
-  );
-  return [posX, posY];
+function isValidIP(ip: string): boolean {
+  return /^(?!.*\.$)((?!0\d)(1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/.test(ip);
 }
 
-console.log(
-  mineLocation([
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 0, 0],
-  ])
-);
+console.log(isValidIP("0.0.0.0"));
+console.log(isValidIP(""));
+console.log(isValidIP("\n1.2.3.4"));
+console.log(isValidIP("01.02.03.04"));
+console.log(isValidIP("1e0.1e1.1e2.2e2"));
+console.log(isValidIP("137.255.156.100"));
+
